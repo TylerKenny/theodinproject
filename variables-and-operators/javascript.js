dfae17f325ba651f1d53f75b3efcc3e5f00715c6
Tyler <tyler.j.kenny9@gmail.com>
2024-12-04 19:07:13 -0400

    completed variables and operators lesson 1

---
+++ variables-and-operators/javascript.js
@@ -0,0 +1,76 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+// Add 2 numbers together
+console.log(23 + 97)
+
+// Add a sequence of 6 different numbers together
+console.log(1 + 2 +  3 + 4 + 5 + 6)
+
+// Print the value of the following expression: 
+let nums = (4 + 6 + 9) / 77
+console.log(nums)
+
+//Let's use variables!
+
+let a = 10
+
+console.log(a)
+9 * a
+let b = 7 * a
+console.log(b)
+
+// Try this sequience:
+
+let max = 57
+let actual = max - 13
+let percentage = actual / max
+console.log(percentage)
+
+// Name three ways to declare a variable
+// let
+// const 
+// var
+
+//Which of the three variable declarations should you avoid and why?
+// var 
+// it was the original way to define variables, however let and const allowed more control over assignment
+//
+
+//what happens when you add numbers and strings together?
+//undefined, probably need a conversion like python
+
+// How does the Modulo (%), or Remainder, operator work?
+// Divides the numbers and gives you the leftover
+//
+// Explain the difference between == and ===.
+// == equal value whereas === is equal value & equal type
+//
+// When would you receive a NaN result?
+// NaN - Not a Number
+
+// Nan is a javascript reserved word indicating that a number is not a legal number.
+
+// Trying to do arithmetic with a non-numeric string will result in nan (not a number):
+//
+// How do you increment and decrement a number?
+// ++ & --
+//
+// Explain the difference between prefixing and postfixing increment/decrement operators.
+// prefixing returns the value with the increment included, postfixing returns the value before the increment/decrement is applied
+//
+// What is operator precedence and how is it handled in JS?
+// PEDMAS basically
+//
+// How do you access developer tools and the console?
+// (Q) firefox, click console, inspect element on any browser though
+//
+// How do you log information to the console?
+// console.log(information)
+// or enter it into the console from the browser
+//
+// What does unary plus operator do to string representations of integers? eg. +”10”
+// nothing 
